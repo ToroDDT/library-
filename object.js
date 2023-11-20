5dd6e4ae333b12c90a53d276730cad89f0de6a00
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2023-11-19 18:27:12 -0800

    create book object constructor
    store input values into an object

---
+++ object.js
@@ -0,0 +1,13 @@
+//create an object constructor for books/:
+    // create a function called book 
+   //create a variable author 
+   //create a variable pages 
+   //create a variable title 
+   // create a variable read 
+
+   function Book () {
+    this.author = author,
+    this.title = title, 
+    this.pages = pages,
+    this.read = read
+  }
