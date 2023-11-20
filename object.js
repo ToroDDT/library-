9b28bf1a403c3be67b53a8046e5a4f0c572c86e8
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2023-11-19 22:28:20 -0800

    create function that adds a book to array/display

--- object.js
+++ object.js
@@ -1,36 +1,49 @@
-
-//create an object constructor for books/:
-    // create a function called book 
-   //create a variable author 
-   //create a variable pages 
-   //create a variable title 
-   // create a variable read 
+const myLibrary = []
 
    function Book (author, title, pages, read) {
     this.author = author,
     this.title = title, 
     this.pages = pages,
-    this.read = read
+    this.read = read,
+    indexNumber = 
   }
 
+    button = document.getElementById("submit");
+    form = document.getElementById("book-form")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        addBookToLibrary()
+    })
     
-  //create a function to add a book to myLibrary array 
-  //we first need to gather input from user 
-  //How do we gather input from user?
-     // create input fields:
-             // how do we gather the text value from each field 
-              //create selectors for each field input 
-             // for each input field get text value 
-      //store each input text value into a variable 
-      //call book constructor function with the following arguements(author,pages,title)
-      //store return value from function into variable called book
-      //add book object to an array called myLibrary
   
-      function addBookToLibrary () {;
+    function addBookToLibrary () {;
         author = document.getElementById("author").value;
         title = document.getElementById("title").value;
         pages = document.getElementById("pages").value;
         read = document.getElementById("read").value;
         let book = new Book(author, title, pages, read)
+        myLibrary.push(book)
+        renderBook()
+        console.log(myLibrary)
+    }
 
-      }
+    function renderBook () {
+        for (let i = 0; i < myLibrary.length; i++){
+            let book = myLibrary[i]
+            let indexNumber = myLibrary.indexOf(book)
+            let div = document.createElement("div");
+            body.appendChild(div).innerHTML = `
+                <div data-index-number = "${indexNumber}" class = "container-card">
+                <div>Author <span>${book.author}</span></div>
+                <div>Title <span>${book.title}</span></div>
+                <div>Pages <span>${book.pages}</span></div>
+                <div>Read? <span>${book.read}</span></div>
+                <button data-index-number = "${indexNumber}" class="remove">Remove</button>
+                </div>`
+        
+                
+        }
+
+    }
+
+    
